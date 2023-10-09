4eea9cf604dade63788290b1a6dede4cbec8b397
sarim <sarim@students.uit.edu>
2023-10-09 12:06:58 +0500

    update login.js

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 // Login a user
 router.post("/login", async (req, res) => {
-  console.log('hello')
+  
   const username = req.body.username;
   const password = req.body.password;
   console.log(username,password)
@@ -23,7 +23,7 @@ router.post("/login", async (req, res) => {
     res.status(401).json({ message: "Invalid password" });
     return;
   }
-  console.log(token)
+
   const token = await generateJWTToken(user);
   res.status(200).json({ token });
 });
@@ -33,7 +33,7 @@ async function generateJWTToken(user) {
   const token = jwt.sign({
     id: user._id,
     username: user.username,
-  }, "Sarimak28", {
+  }, "sarimak28", {
     expiresIn: "1h",
   });
 
